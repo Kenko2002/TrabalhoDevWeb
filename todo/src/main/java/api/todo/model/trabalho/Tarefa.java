6e1a608ace59805e38f444f9533382f9c7436d04
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-12-11 10:21:26 -0300

    Add files via upload

---
+++ todo/src/main/java/api/todo/model/trabalho/Tarefa.java
@@ -0,0 +1,65 @@
+package api.todo.model.trabalho;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Entity
+@Getter
+@Setter
+public class Tarefa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+    private String tipo;
+	
+	@Column
+    private String descricao;
+	
+	@Column
+    private String nome;
+
+
+	@OneToMany(mappedBy = "tarefas", cascade = CascadeType.ALL)
+	private List<HistoriaDeUsuario> historias2 = new ArrayList<>();
+
+
+
+
+	public Tarefa(){
+
+	}
+	
+	public Tarefa(String nome, String tipo, String descricao) {
+		this.nome=nome;
+		this.tipo=tipo;
+		this.descricao=descricao;
+	}
+	
+/*	public List<HistoriaDeUsuario> addHistoria(HistoriaDeUsuario historia){
+		this.historias.add(historia);
+		historia.getTarefas().add(this);
+		return this.historias;
+	}*/
+}
+
+
