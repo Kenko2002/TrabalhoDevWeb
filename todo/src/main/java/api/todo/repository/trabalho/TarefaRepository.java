6913a8aac9224869a89bfce2096f4a1f4b8ec304
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-12-12 21:28:53 -0300

    Delete todo directory

--- todo/src/main/java/api/todo/repository/trabalho/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package api.todo.repository.trabalho;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import api.todo.model.trabalho.Tarefa;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
