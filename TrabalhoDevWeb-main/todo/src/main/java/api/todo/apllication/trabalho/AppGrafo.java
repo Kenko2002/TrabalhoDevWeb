67ee52f7e2631a769605aff8eccc006c1563d3fb
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-12-14 12:54:53 -0300

    Update AppGrafo.java

--- TrabalhoDevWeb-main/todo/src/main/java/api/todo/apllication/trabalho/AppGrafo.java
+++ TrabalhoDevWeb-main/todo/src/main/java/api/todo/apllication/trabalho/AppGrafo.java
@@ -5,8 +5,11 @@ import api.todo.grafo.Vertice;
 import api.todo.model.trabalho.Epico;
 import api.todo.model.trabalho.HistoriaDeUsuario;
 import api.todo.model.trabalho.Tarefa;
+import api.todo.repository.trabalho.HistoriaDeUsuarioRepository;
 import lombok.Getter;
 import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +18,9 @@ import java.util.List;
 @Setter
 @Service
 public class AppGrafo {
+
+    @Autowired
+	private HistoriaDeUsuarioRepository repository_hist;
 
     private Grafo<Object> grafo = new Grafo<>();
 
@@ -33,7 +39,7 @@ public class AppGrafo {
     }
 
     private void adicionarDependenciasHistorias(Epico epico) {
-        for (HistoriaDeUsuario historia : epico.getHistoriadeusuarios()) {
+        for (HistoriaDeUsuario historia : getHistoriadeusuarios(epico)) {
             adicionarDependenciasTarefas(historia);
             grafo.adicionarAresta(epico, historia, 1);
         }
@@ -58,4 +64,18 @@ public class AppGrafo {
     public Grafo<Object> getGrafo() {
         return grafo;
     }
+
+
+
+
+
+
+    public List<HistoriaDeUsuario> getHistoriadeusuarios(Epico epico) {
+        Long epicoId = epico.getId(); // Supondo que Epico tem um método getId()
+
+        // Utilize o método findAllByEpicoId do seu repository
+        return repository_hist.findAllByEpicoId(epicoId);
+    }
+
 }
+
